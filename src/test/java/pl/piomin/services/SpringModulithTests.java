ea33984bef42c0c5adc6ee0e083fabd6140fbffa
piomin <piotr.minkowski@gmail.com>
2023-10-11 11:49:44 +0200

    add documentation generator + actuator + observability

--- src/test/java/pl/piomin/services/SpringModulithTests.java
+++ src/test/java/pl/piomin/services/SpringModulithTests.java
@@ -2,11 +2,22 @@ package pl.piomin.services;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.modulith.core.ApplicationModules;
+import org.springframework.modulith.docs.Documenter;
 
 public class SpringModulithTests {
 
+    ApplicationModules modules = ApplicationModules.of(SpringModulith.class);
+
     @Test
     void shouldBeCompliant() {
-        ApplicationModules.of(SpringModulith.class).verify();
+        modules.verify();
+    }
+
+    @Test
+    void writeDocumentationSnippets() {
+        new Documenter(modules)
+                .writeModuleCanvases()
+                .writeModulesAsPlantUml()
+                .writeIndividualModulesAsPlantUml();
     }
 }
