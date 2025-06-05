7dcab335d00d7300022486eec6f1052745182c71
piomin <piotr.minkowski@gmail.com>
2025-06-06 00:14:05 +0200

    Fixes + updates

--- src/main/java/pl/piomin/services/employee/management/EmployeeManagement.java
+++ src/main/java/pl/piomin/services/employee/management/EmployeeManagement.java
@@ -2,7 +2,7 @@ package pl.piomin.services.employee.management;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.modulith.ApplicationModuleListener;
+import org.springframework.modulith.events.ApplicationModuleListener;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import pl.piomin.services.OrganizationRemoveEvent;
